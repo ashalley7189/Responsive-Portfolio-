ddc36d3118d1a958ffe32f8e7d86982970264734
Ashleigh Kessel <AshleighKessel@gmail.com>
2018-09-27 11:57:53 -0700

    Menu Button, JS & Rotate

---
+++ dist/js/main.js
@@ -0,0 +1,39 @@
+//Select DOM Items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".btn-nav");
+const menuBranding = document.querySelector(".menu-branding");
+
+const navItems = document.querySelectorAll(".nav-item");
+
+//Set Initial Sate of Menu
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBtn.classList.add("show");
+    navItems.forEach(navItem => {
+      navItem.classList.add("show");
+    });
+
+    //Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBtn.classList.remove("show");
+    navItems.forEach(navItem => {
+      navItem.classList.remove("show");
+    });
+
+    //Set Menu State
+    showMenu = false;
+  }
+}
