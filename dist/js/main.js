eeb72225212125c31076e9721ea0bce10070b19b
Ashleigh Kessel <AshleighKessel@gmail.com>
2018-09-27 18:58:40 -0700

    added drop down nav bar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,7 @@
 //Select DOM Items
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
-const menuNav = document.querySelector(".btn-nav");
+const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 
 const navItems = document.querySelectorAll(".nav-item");
